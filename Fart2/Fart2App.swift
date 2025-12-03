55a745af16e093db70152d2fad8c6db7a9565ce7
Paul McGrath <gingermcninja@gmail.com>
2025-12-03 16:41:33 -0500

    Initial Commit

---
+++ Fart2/Fart2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fart2App.swift
+//  Fart2
+//
+//  Created by Paul McGrath on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Fart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
